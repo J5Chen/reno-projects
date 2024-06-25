69600c233b0de6784b6e83febea8da95d1c578c9
Jimmy <jimmyofchen@hotmail.com>
2024-06-25 00:30:19 -0400

    Updates

--- assets/browser-h_2VTXOJ.js
+++ assets/browser-h_2VTXOJ.js
@@ -1,4 +1,4 @@
-import{c as Pe,g as Xe}from"./index-DRXHxGOy.js";var se={exports:{}};/** @license
+import{c as Pe,g as Xe}from"./index-CbUaZo3U.js";var se={exports:{}};/** @license
  * eventsource.js
  * Available under MIT License (MIT)
  * https://github.com/Yaffle/EventSource/
